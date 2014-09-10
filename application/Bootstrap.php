b467b1610a8e5d043d3f3533dba7ea5c67b97767
Jun <Jun@Juns-MacBook-Air.local>
2014-09-10 22:13:31 +0100

    add logger.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,7 +1,29 @@
 <?php
 
+Zend_Loader::loadClass('Zend_Registry');
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+
+	# -------
+
+    protected function _initRegistry()
+    {
+        $config = new Zend_Config($this->getOptions());
+        Zend_Registry::set('config', $config);
+
+        // $resource = $this->getPluginResource('db');
+        // $db = $resource->getDbAdapter();
+        // $db->setFetchMode(Zend_Db::FETCH_ASSOC);
+        // Zend_Registry::set('db', $db);
+    }
+
+
+	protected function _initRoutes()
+    {
+    	// set up baseUrl
+        $config = new Zend_Config($this->getOptions());
+        Application_Model_Logger::log('_initRoutes');
+    }
 
 
 }
